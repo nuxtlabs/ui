69719dfee3852ae5ab828882e5c7111b44f798d7
Chingiz Mammadov <info@chz.dev>
2025-01-31 00:21:24 +0400

    feat(locale) Add Azerbaijani language added to docs

--- src/runtime/locale/az.ts
+++ src/runtime/locale/az.ts
@@ -11,7 +11,7 @@ export default defineLocale({
     },
     calendar: {
       prevYear: 'Əvvəlki il',
-      nextYear: 'Növbəti il', 
+      nextYear: 'Növbəti il',
       prevMonth: 'Əvvəlki ay',
       nextMonth: 'Növbəti ay'
     },
