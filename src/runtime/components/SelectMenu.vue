239b745d51d596a9c6002210be39a46edd430b03
Emils Gulbis <emils.gulbis@gmail.com>
2025-02-05 12:45:35 +0200

    feat(SelectMenu): add clearable icon to reset modelValue

--- src/runtime/components/SelectMenu.vue
+++ src/runtime/components/SelectMenu.vue
@@ -46,6 +46,11 @@ export interface SelectMenuProps<T extends MaybeArrayOfArrayItem<I>, I extends M
   size?: SelectMenuVariants['size']
   required?: boolean
   /**
+   * Determines if user can clear the `modelValue` with icon click
+   * @defaultValue false
+   */
+  clearable?: boolean
+  /**
    * The icon displayed to open the menu.
    * @defaultValue appConfig.ui.icons.chevronDown
    */
@@ -55,6 +60,11 @@ export interface SelectMenuProps<T extends MaybeArrayOfArrayItem<I>, I extends M
    * @defaultValue appConfig.ui.icons.check
    */
   selectedIcon?: string
+  /**
+   * The icon displayed to clear the value.
+   * @defaultValue appConfig.ui.icons.close
+   */
+  clearIcon?: string
   /**
    * The content of the menu.
    * @defaultValue { side: 'bottom', sideOffset: 8, collisionPadding: 8, position: 'popper' }
@@ -152,7 +162,8 @@ const props = withDefaults(defineProps<SelectMenuProps<T, I, V, M>>(), {
   portal: true,
   searchInput: true,
   labelKey: 'label' as never,
-  resetSearchTermOnBlur: true
+  resetSearchTermOnBlur: true,
+  clearable: false
 })
 const emits = defineEmits<SelectMenuEmits<T, V, M>>()
 const slots = defineSlots<SelectMenuSlots<T, M>>()
@@ -200,6 +211,14 @@ function displayValue(value: T | T[]): string {
   return item && (typeof item === 'object' ? get(item, props.labelKey as string) : item)
 }
 
+const isEmpty = computed(() => {
+  if (Array.isArray(props.modelValue)) {
+    return props.modelValue.length === 0
+  }
+
+  return !(props.modelValue)
+})
+
 const groups = computed(() => props.items?.length ? (Array.isArray(props.items[0]) ? props.items : [props.items]) as SelectMenuItem[][] : [])
 // eslint-disable-next-line vue/no-dupe-keys
 const items = computed(() => groups.value.flatMap(group => group) as T[])
@@ -244,6 +263,7 @@ function onUpdate(value: any) {
   if (toRaw(props.modelValue) === value) {
     return
   }
+  console.log('update', value)
   // @ts-expect-error - 'target' does not exist in type 'EventInit'
   const event = new Event('change', { target: { value } })
   emits('change', event)
@@ -275,6 +295,11 @@ function onUpdateOpen(value: boolean) {
     emitFormFocus()
     clearTimeout(timeoutId)
   }
+}
+
+function onClear() {
+  const newValue = props.multiple ? [] : null
+  emits('update:modelValue', newValue as SelectModelValue<T, V, M>)
 }
 </script>
 
@@ -328,6 +353,7 @@ function onUpdateOpen(value: boolean) {
         </slot>
 
         <span v-if="isTrailing || !!slots.trailing" :class="ui.trailing({ class: props.ui?.trailing })">
+          <UIcon v-if="props.clearable && !isEmpty" :name="clearIcon || appConfig.ui.icons.close" :class="ui.clearIcon({ class: props.ui?.clearIcon })" @click.prevent.stop="onClear()" />
           <slot name="trailing" :model-value="(modelValue as M extends true ? T[] : T)" :open="open" :ui="ui">
             <UIcon v-if="trailingIconName" :name="trailingIconName" :class="ui.trailingIcon({ class: props.ui?.trailingIcon })" />
           </slot>
