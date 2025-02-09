ccdf32fdf5d342116544c311970643cc67007404
Eugen Istoc <eugenistoc@gmail.com>
2025-02-09 18:23:51 -0500

    chore(modal): add nested modal example with TypeScript setup

--- src/devtools/runtime/examples/ModalExample.vue
+++ src/devtools/runtime/examples/ModalExample.vue
@@ -1,8 +1,19 @@
+<script lang="ts" setup>
+import ModelExample2 from './ModalExample2.vue'
+
+const modal = useModal()
+
+const onOpenNested = () => {
+  modal.open(ModelExample2, {
+    description: 'Nested',
+
+    dismissible: false
+  })
+}
+</script>
+
 <template>
-  <UModal>
-    <UButton label="Open Modal" />
-    <template #content>
-      <div class="h-72" />
-    </template>
-  </UModal>
+  <div>
+    asdf
+  </div>
 </template>
