820f8577e5f688d2e7c47a7737ca6025d009ce55
HugoRCD <hugo.richard@epitech.eu>
2025-02-11 10:55:01 +0100

    up

--- build.config.ts
+++ build.config.ts
@@ -20,5 +20,5 @@ export default defineBuildConfig({
       options.addRelativeDeclarationExtensions = false
     }
   },
-  externals: ['#build/ui', '#build/ui.css', '#build/app.config', 'vite']
+  externals: ['#build/ui', '#build/app.config', 'vite']
 })
